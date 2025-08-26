777412999445710ea897d634d43fd99588513254
haoly1989 <haoly1989@noreply.gitcode.com>
2025-08-26 09:03:31 +0800

    任务1-搭建React应用

---
+++ web/src/App.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+
+function App() {
+  return (
+    <div className="flex justify-center items-center text-3xl min-h-screen">
+      <Button>Posts App</Button>
+    </div>
+  );
+}
+
+export default App;
