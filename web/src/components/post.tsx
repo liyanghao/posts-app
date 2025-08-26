399c412756e61d9ab3283b029b85d47231e77ca3
haoly1989 <haoly1989@noreply.gitcode.com>
2025-08-26 11:10:05 +0800

    重构Post组件-添加PostActions组件

--- web/src/components/post.tsx
+++ web/src/components/post.tsx
@@ -1,6 +1,5 @@
 import type { PostType } from "@/data/types";
-import { Button } from "@/components/ui/button";
-import { TrashIcon, Pencil2Icon } from "@radix-ui/react-icons";
+import PostActions from "./post-actions";
 
 const Post = ({ post }: { post: PostType }) => {
   return (
@@ -15,14 +14,7 @@ const Post = ({ post }: { post: PostType }) => {
             minute: "numeric",
           })}
         </div>
-        <div className="flex justify-end">
-          <Button variant={"ghost"} size={"icon"}>
-            <Pencil2Icon className="w-4 h-4" />
-          </Button>
-          <Button variant={"ghost"} size={"icon"}>
-            <TrashIcon className="w-4 h-4" />
-          </Button>
-        </div>
+        <PostActions />
       </div>
       <p className="p-4">{post.content}</p>
     </div>
